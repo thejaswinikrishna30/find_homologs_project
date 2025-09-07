df54c4bb7b4ce8176d7b3c5ced536ff744608dea
thejaswinikrishna30 <thejaswinikrishna30@gmail.com>
2025-09-07 12:34:40 -0400

    Initial script

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,1 +1,21 @@
-#final
+#!/bin/bash
+
+query="$1"
+subject="$2"
+output="$3"
+
+makeblastdb -in "$subject" -dbtype prot -out subject_db
+
+blastx -query "$query" -db subject_db \
+       -outfmt "6 pident length qlen bitscore" \
+       -out temp_blast_results.txt
+
+awk '{
+    coverage = $2 / $3 * 100    # $2 = alignment length, $3 = query length
+    if ($1 >= 30 && coverage >= 90) print
+}' temp_blast_results.txt > filtered_results.txt
+
+num_hits=$(wc -l < "$output")
+echo "No. of hits: $num_hits"
+
+rm temp_blast_results.txt
