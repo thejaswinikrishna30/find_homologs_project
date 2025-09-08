1f0c6674ec82fd7b0c4ada7264f2aabada6723e0
thejaswinikrishna30 <thejaswinikrishna30@gmail.com>
2025-09-08 00:31:46 -0400

    final code

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,11 +4,7 @@ subject="$2"
 output="$3"
 makeblastdb -in "$subject" -dbtype nucl -out subject_db
 tblastn -query "$query" -db subject_db \
-       -outfmt "6 qseqid sseqid pident length qlen bitscore" \
-       |  awk -F'\t' '$5>0 && ($3>30) && ($4/$5>0.9)' > "$output"
+       -outfmt "6 qseqid sseqid pident length qlen  bitscore" \
+       | awk -F'\t' '$5>0 && ($3>30) && ($4/$5>0.9)' > "$output"
 
 num_hits=$(wc -l < "$output")
-
-
-
-
