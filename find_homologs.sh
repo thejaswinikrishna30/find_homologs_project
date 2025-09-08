d360f088eaebed70589c4109e634ae82a178ccf9
thejaswinikrishna30 <thejaswinikrishna30@gmail.com>
2025-09-08 00:28:04 -0400

    final code

--- find_homologs.sh
+++ find_homologs.sh
@@ -5,7 +5,7 @@ output="$3"
 makeblastdb -in "$subject" -dbtype nucl -out subject_db
 tblastn -query "$query" -db subject_db \
        -outfmt "6 qseqid sseqid pident length qlen bitscore" \
-       | awk '($3 >= 30 && $6 >= 90)' > "$output"
+       |  awk -F'\t' '$5>0 && ($3>30) && ($4/$5>0.9)' > "$output"
 
 num_hits=$(wc -l < "$output")
 
