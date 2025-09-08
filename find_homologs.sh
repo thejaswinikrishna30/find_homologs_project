bd32120b55123e0fa59d3d1f1e4046e665c16e65
KR Thejaswini Krishna <thejaswinikrishna30@gmail.com>
2025-09-08 16:48:16 -0400

    Update find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 query="$1"
 subject="$2"
-output="$3"
 makeblastdb -in "$subject" -dbtype nucl -out subject_db
 tblastn -query "$query" -db subject_db \
        -outfmt "6 qseqid sseqid pident length evalue bitscore" \
   | awk -F'\t' '($3 > 30) && ($4 > 0)' \
-  > "$output"
-num_hits=$(wc -l < "$output")
-echo "$num_hits"
+  | grep -c .
+
