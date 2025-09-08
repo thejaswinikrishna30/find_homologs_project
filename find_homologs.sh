9ceb6feb2af75e0bf05d2c8229089c1d224abb13
KR Thejaswini Krishna <thejaswinikrishna30@gmail.com>
2025-09-08 19:08:35 -0400

    Update find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 query="$1"
 subject="$2"
-makeblastdb -in "$subject" -dbtype nucl -out subject_db
-tblastn -query "$query" -db subject_db \
-       -outfmt "6 qseqid sseqid pident length evalue bitscore" \
-  | awk -F'\t' '($3 > 30) && ($4 > 0)' \
-  | grep -c .
+output="$3"
 
+makeblastdb -in "$subject" -dbtype nucl -out subject_db
+
+tblastn -query "$query" -db subject_db \
+       -outfmt "6 qseqid sseqid pident length qlen bitscore" \
+        | awk -F'\t' '$5>0 && ($3>30) && ($4/$5>0.9)' > "$output"
+
+wc -l < "$output"
