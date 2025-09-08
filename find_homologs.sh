0a768226d0db5ccc097293e728482710d088119e
KR Thejaswini Krishna <thejaswinikrishna30@gmail.com>
2025-09-08 16:33:24 -0400

    Update find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
+  GNU nano 7.2                  find_homologs.sh                            #!/bin/bash
 query="$1"
 subject="$2"
 output="$3"
-makeblastdb -in "$subject" -dbtype nucl -out subject_db
-tblastn -query "$query" -db subject_db \
-       -outfmt "6 qseqid sseqid pident length qlen bitscore" \
-       |  awk -F'\t' '$5>0 && ($3>30) && ($4/$5>0.9)' > "$output"
-
+makeblastdb -in "$subject" -dbtype prot -out subject_db
+blastx -query "$query" -db subject_db \
+       -outfmt "6 qseqid sseqid pident length evalue bitscore" \
+       | awk -F'\t' '$5>0 && ($3>30) && ($4/$5>0.9)' > "$output"
 num_hits=$(wc -l < "$output")
 echo "$num_hits"
-
-
 
 
